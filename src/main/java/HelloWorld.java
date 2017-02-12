7e9a2c61df2642bbc60f91a4156eae106961f91a
Farwatch <nick.william.baker@gmail.com>
2017-02-12 21:39:01 +0000

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by nick on 12/02/17.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by nick on 12/02/17.
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void shouldBeTrue(){
+        assertThat(true).isTrue();
+    }
+
+}
