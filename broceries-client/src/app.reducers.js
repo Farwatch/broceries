877fa87a0e83a4181fbeba6d48f07f7f524b8654
Farwatch <nick.william.baker@gmail.com>
2017-09-09 15:36:17 +0100

    fixes some stuff from previous WiP patch. updates stuff

--- broceries-client/src/app.reducers.js
+++ broceries-client/src/app.reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import ingredientListReducers from 'ingredient_list/ingredient_list.reducer'
+import ingredientListReducers from './ingredient_list/ingredient_list.reducer'
 
 export default combineReducers({
     ingredientList: ingredientListReducers
--- broceries-client/src/ingredient_list/ingredient_list.reducer.js
+++ broceries-client/src/ingredient_list/ingredient_list.reducer.js
@@ -3,3 +3,4 @@ export const getInitialState = () => initialState
 const initialState = {
 }
 
+export default () => initialState
